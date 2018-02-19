419e8329bb7cb4901425a2a6fc53aff2bb27f490
Hawken Rives <rives@stolaf.edu>
2018-02-19 09:58:49 -0600

    reindent

--- read-enroll.py
+++ read-enroll.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
-''' Sonia Moreno, 9/2017
+""" Sonia Moreno, 9/2017
  Scrapes data from Carleton Enroll website containing course schedule information.
- '''
+ """
 from __future__ import print_function
 from collections import defaultdict
 from bs4 import BeautifulSoup
@@ -11,154 +11,153 @@ import csv
 import sys
 import json
 
+
 def Academic_Term():
-        ''' Returns list of academic terms that user can choose from. Item in list
-        will be passed to function that returns html link with term info provided.
-        Example: 'term=18WI' in 'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=18WI&subject=CS'
-        '''
-	# Homepage showing listings of academic terms and course subjects
-	html_enroll = requests.get('https://apps.carleton.edu/campus/registrar/schedule/enroll/').text
-	soup2 = BeautifulSoup(html_enroll, 'html5lib')
+    """ Returns list of academic terms that user can choose from. Item in list
+    will be passed to function that returns html link with term info provided.
+    Example: 'term=18WI' in 'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=18WI&subject=CS'
+    """
+    # Homepage showing listings of academic terms and course subjects
+    html_enroll = requests.get('https://apps.carleton.edu/campus/registrar/schedule/enroll/').text
+    soup2 = BeautifulSoup(html_enroll, 'html5lib')
 
-	# Tag object containing list of academic terms 
-	term_summary = soup2.find("select", id = "termElement")
+    # Tag object containing list of academic terms
+    term_summary = soup2.find("select", id="termElement")
 
-	# Each term name such as "Winter 2018" has tag "option"
-	terms = term_summary.find_all("option")
+    # Each term name such as "Winter 2018" has tag "option"
+    terms = term_summary.find_all("option")
 
-	# We want the value attribute; example: <option value="18WI">
-	# Create list with all value attributes; this will be list of terms available to choose from
-	term_list = []
-	for option in terms:
-		term_list.append(option['value'])
-	return term_list
-
-
-def Subject(): 
-        ''' Returns list of course subjects. Each will be passed to function that returns
-        appropriate html link which contains specific course information for the subject
-        Example: 'subject=CS' in 'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=18WI&subject=CS'
-        '''
-	html_enroll = requests.get('https://apps.carleton.edu/campus/registrar/schedule/enroll/').text
-	soup2 = BeautifulSoup(html_enroll, 'html5lib')
+    # We want the value attribute; example: <option value="18WI">
+    # Create list with all value attributes; this will be list of terms available to choose from
+    term_list = []
+    for option in terms:
+        term_list.append(option['value'])
+    return term_list
 
 
-	# Tag object containing list of subjects
-	subject_summary = soup2.find("select", id = "subjectElement")
+def Subject():
+    """ Returns list of course subjects. Each will be passed to function that returns
+    appropriate html link which contains specific course information for the subject
+    Example: 'subject=CS' in 'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=18WI&subject=CS'
+    """
+    html_enroll = requests.get('https://apps.carleton.edu/campus/registrar/schedule/enroll/').text
+    soup2 = BeautifulSoup(html_enroll, 'html5lib')
 
-	# Each subject within summary has tag "option"
-	# Create a list with subjects, excluding 'Selected' tag (1st item)
-	subjects = subject_summary.find_all("option")[1:]
+    # Tag object containing list of subjects
+    subject_summary = soup2.find("select", id="subjectElement")
 
-	# Only get the associated text, excluding the tag itself and add them to list
-	subj_list = []
-	for item in subjects:
-		subj_list.append(item.get_text())
+    # Each subject within summary has tag "option"
+    # Create a list with subjects, excluding 'Selected' tag (1st item)
+    subjects = subject_summary.find_all("option")[1:]
 
-	# print subj_list
+    # Only get the associated text, excluding the tag itself and add them to list
+    subj_list = []
+    for item in subjects:
+        subj_list.append(item.get_text())
 
-	# Each item in subj_list is currently in the form: 'Computer Science (CS)'
-	# We only want the abbrevation in the parentheses so that we can use this in the html link
-	# We use regular expressions to achieve this.
-	subj_abbrev = []
-	for i in subj_list:
-		subj_abbrev.append(re.search('\((.*?)\)', i).group(1))
+    # print subj_list
 
-	# print subj_abbrev
-		
-	return subj_abbrev
+    # Each item in subj_list is currently in the form: 'Computer Science (CS)'
+    # We only want the abbrevation in the parentheses so that we can use this in the html link
+    # We use regular expressions to achieve this.
+    subj_abbrev = []
+    for i in subj_list:
+        subj_abbrev.append(re.search('\((.*?)\)', i).group(1))
+
+    # print subj_abbrev
+
+    return subj_abbrev
 
 
 def Specific_Course_Info(term):
-        ''' Returns dict object with course number, course name, and start/end times for each course
-        Finds course info based on the academic term and subject chosen (in this case, Winter 2018)
-        '''
-	# Creates dict object with course number as key and list containing name and times for course as values
-	course_info = defaultdict(list)
+    """ Returns dict object with course number, course name, and start/end times for each course
+    Finds course info based on the academic term and subject chosen (in this case, Winter 2018)
+    """
+    # Creates dict object with course number as key and list containing name and times for course as values
+    course_info = defaultdict(list)
 
-	subjects = Subject()
+    subjects = Subject()
 
-	for subject in subjects:
-		html_string = 'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=' + term + '&subject=' + subject
+    for subject in subjects:
+        html_string = 'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=' + term + '&subject=' + subject
 
-		# Course listings for subject during term provided
-		html = requests.get(html_string).text
+        # Course listings for subject during term provided
+        html = requests.get(html_string).text
 
-		soup = BeautifulSoup(html, 'html5lib')
+        soup = BeautifulSoup(html, 'html5lib')
 
-		# Creates list of all items with course as class attribute, excluding related courses
-		course_summary = soup.find_all("div", class_="course")
-		for course in course_summary:
-			course_num = course.find(class_= "coursenum").get_text()
-			# Finds title attribute within each course
-			title = course.find(class_ = "title").get_text()
-			# Only takes the actual name of the course
-			# which is next to the coursenum attribute but not within its own tag
-			for item in title:
-				course_name = course.find(class_= "coursenum").next_sibling	
+        # Creates list of all items with course as class attribute, excluding related courses
+        course_summary = soup.find_all("div", class_="course")
+        for course in course_summary:
+            course_num = course.find(class_="coursenum").get_text()
+            # Finds title attribute within each course
+            title = course.find(class_="title").get_text()
+            # Only takes the actual name of the course
+            # which is next to the coursenum attribute but not within its own tag
+            for item in title:
+                course_name = course.find(class_="coursenum").next_sibling
 
-			# Add info to list associated with key
-			specific_info = {}
-			if course_num.find(subject) > -1:
-				specific_info['course_num'] = course_num
-				specific_info['title'] = course_name
-				if course.find(class_ = "faculty") != None:
-					faculty = course.find(class_ = "faculty").get_text()
-					specific_info['faculty'] = faculty
-					if course.find(class_ ="faculty").next_sibling != None:
-                                                summary = course.find(class_ = "faculty").next_sibling
-                                                summary = summary.encode("utf-8")
-                                                specific_info['summary'] = summary
-					else:
-                                                specific_info['summary'] = "n/a"
-				else:
-					specific_info['faculty'] = "n/a"
-				if course.find(class_ = "status") != None:
-					enrollment = course.find(class_ = "status").get_text()
-					# specific_info['enrollment'] = enrollment
-					registered = re.findall(r'(?<=Registered: ).*?(?=\,)', enrollment)[0]
-					size = re.findall(r'(?<=Size: ).*?(?=\,)', enrollment)[0]
-					# print registered
-					specific_info['registered'] = registered
-					specific_info['size'] = size
-					# print enrollment
-				else:
-					specific_info['registered'] = "n/a"
-					specific_info['size'] = "n/a"
+            # Add info to list associated with key
+            specific_info = {}
+            if course_num.find(subject) > -1:
+                specific_info['course_num'] = course_num
+                specific_info['title'] = course_name
+                if course.find(class_="faculty") != None:
+                    faculty = course.find(class_="faculty").get_text()
+                    specific_info['faculty'] = faculty
+                    if course.find(class_="faculty").next_sibling != None:
+                        summary = course.find(class_="faculty").next_sibling
+                        summary = summary.encode("utf-8")
+                        specific_info['summary'] = summary
+                    else:
+                        specific_info['summary'] = "n/a"
+                else:
+                    specific_info['faculty'] = "n/a"
+                if course.find(class_="status") != None:
+                    enrollment = course.find(class_="status").get_text()
+                    # specific_info['enrollment'] = enrollment
+                    registered = re.findall(r'(?<=Registered: ).*?(?=\,)', enrollment)[0]
+                    size = re.findall(r'(?<=Size: ).*?(?=\,)', enrollment)[0]
+                    # print registered
+                    specific_info['registered'] = registered
+                    specific_info['size'] = size
+                # print enrollment
+                else:
+                    specific_info['registered'] = "n/a"
+                    specific_info['size'] = "n/a"
 
+                # course_info[0].append({})
+                # Start and end times for courses that have set times
+                # Account for classes without set times
+                if course.find(class_="start") != None:
+                    start_time = course.find("span", {"class": "start"}).get_text()
+                    end_time = course.find(class_="end").get_text()
+                    specific_info['start_time'] = start_time
+                    specific_info['end_time'] = end_time
 
-				# course_info[0].append({})	
-				# Start and end times for courses that have set times
-				# Account for classes without set times
-				if course.find(class_ = "start") != None:
-					start_time = course.find("span", {"class": "start"}).get_text()
-					end_time = course.find(class_ = "end").get_text()
-					specific_info['start_time'] = start_time
-					specific_info['end_time'] = end_time
+                else:
+                    specific_info['start_time'] = "n/a"
+                    specific_info['end_time'] = "n/a"
+                # course_info[0][0].append(start_time)
+                course_info['course_info'].append(specific_info)
+            # course_info[0][0].append(end_time)
 
-				else:
-					specific_info['start_time'] = "n/a"
-					specific_info['end_time'] = "n/a"
-					# course_info[0][0].append(start_time)
-				course_info['course_info'].append(specific_info)
-					# course_info[0][0].append(end_time)
-	
-	# Creates csv file with course info
-	# with open('course_info7.csv', 'w') as f: 
-	# 	w = csv.DictWriter(f, course_info.keys())
-	# 	w.writeheader()
-	# 	w.writerow(course_info)
+    # Creates csv file with course info
+    # with open('course_info7.csv', 'w') as f:
+    # 	w = csv.DictWriter(f, course_info.keys())
+    # 	w.writeheader()
+    # 	w.writerow(course_info)
 
-	# output_file = open('courses_table.csv', 'w')
-	# writer = csv.writer(output_file)
-	# for course in course_info['course_info']:
-	# 	course_row = [course['course_num'].encode("utf-8"), course['title'].encode("utf-8"), course['start_time'].encode("utf-8"), course['end_time'].encode("utf-8")]
-	# 	writer.writerow(course_row)
-	# output_file.close()
-	# print course_info
-	with open('course_data.json', 'w') as fp:
-		json.dump(course_info, fp)
-	return course_info
+    # output_file = open('courses_table.csv', 'w')
+    # writer = csv.writer(output_file)
+    # for course in course_info['course_info']:
+    # 	course_row = [course['course_num'].encode("utf-8"), course['title'].encode("utf-8"), course['start_time'].encode("utf-8"), course['end_time'].encode("utf-8")]
+    # 	writer.writerow(course_row)
+    # output_file.close()
+    # print course_info
+    with open('course_data.json', 'w') as fp:
+        json.dump(course_info, fp)
+    return course_info
 
 
 # def Append_Dicts(a_dict, b_dict):
@@ -169,24 +168,24 @@ def Specific_Course_Info(term):
 
 
 def Generate_HTML():
-        ''' Returns HTML string that Specific Course Info will use to provide information
-        for every term and subject combination.
-        '''
-	terms = Academic_Term()[1:3]
-	subjects = Subject()[1:3]
-	html = []
-	for term in terms:
-		for subject in subjects:
-			print(term)
-			print(subject)
-		Specific_Course_Info(term, subject)
+    """ Returns HTML string that Specific Course Info will use to provide information
+    for every term and subject combination.
+    """
+    terms = Academic_Term()[1:3]
+    subjects = Subject()[1:3]
+    html = []
+    for term in terms:
+        for subject in subjects:
+            print(term)
+            print(subject)
+            Specific_Course_Info(term, subject)
 
 
 def main():
-	
-	# Academic_Term()
-	# Subject()
-	# Generate_HTML()
-	Specific_Course_Info('18WI')
+    # Academic_Term()
+    # Subject()
+    # Generate_HTML()
+    Specific_Course_Info('18WI')
+
 
 main()
