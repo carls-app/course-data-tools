c423ab6d429abbd1611a329e36a0a9faf60c17ba
Hawken Rives <rives@stolaf.edu>
2018-02-19 15:00:23 -0600

    update extractor; turn parser into three phases
    fetch, extract, bundle

--- read-enroll.py
+++ read-enroll.py
@@ -5,10 +5,13 @@
 
 from bs4 import BeautifulSoup
 from argparse import ArgumentParser
+from urllib.parse import parse_qs
 import requests
 import re
+import sys
 import time
 import json
+from pathlib import Path
 import datetime
 
 
@@ -32,13 +35,13 @@ def discover_terms(*, first, last):
     for y in range(first_year, last_year + 1):
         terms = term_names
 
-        if y == first_year and first_sem and first_sem != terms[0]:
+        if y == first_year and first_sem:
             terms = term_names[term_names.index(first_sem):]
-        elif y == last_year and last_sem and last_sem != terms[-1]:
+        elif y == last_year and last_sem:
             terms = term_names[:term_names.index(last_sem)]
 
         for t in terms:
-            yield f'{str(y)}{t}'
+            yield f'{str(y)[2:]}{t}'
 
 
 def fetch_academic_terms():
@@ -96,84 +99,149 @@ def fetch_subjects():
     return subj_abbrev
 
 
-def fetch_courses(*, term, subject):
-    """ Returns dict object with course number, course name, and start/end times for each course
-    Finds course info based on the academic term and subject chosen (in this case, Winter 2018)
-    """
-    # Creates dict object with course number as key and list containing name and times for course as values
-    course_info = []
+def process_course(course):
+    course_num = course.select_one(".coursenum")
+    course_id = course_num.get('title')
 
+    # Split apart the deptnum
+    subject, number = course_num.get_text().split(' ')
+
+    # Finds title attribute within each course
+    # Only takes the actual name of the course, which is next to the coursenum attribute
+    # but not within its own tag
+    title = course.select_one(".title .coursenum").next_sibling.strip()
+
+    # Find the teachers
+    if course.select(".faculty"):
+        instructors = [inst.get_text().strip() for inst in course.select(".faculty a")]
+    else:
+        instructors = []
+
+    # Get the course summary, if possible
+    if course.select_one(".faculty") and course.select_one(".faculty").next_sibling:
+        summary = str(course.select_one(".faculty").next_sibling)
+    elif course.select_one('.prereq') and course.select_one('.prereq').previous_sibling:
+        summary = str(course.select_one('.prereq').previous_sibling)
+    elif course.select_one('.description'):
+        summary = str(course.select_one('.description'))
+    else:
+        summary = None
+
+    # Pull out the prereqs
+    prereq = str(course.select_one('.prereq')) or None
+
+    # and the comments
+    comments = str(course.select_one('.comments')) or None
+
+    # Extract the course status
+    status = course.select_one('.statusName').get_text()
+
+    if course.select_one(".statusName").next_sibling:
+        status_text = str(course.select_one(".statusName").next_sibling).strip()
+        total_size = int(re.search(r'Size: (\d+)', status_text).group(1))
+        registered = int(re.search(r'Registered: (\d+)', status_text).group(1))
+        waitlist = int(re.search(r'Waitlist: (\d+)', status_text).group(1))
+        size = {'total': total_size, 'registered': registered, 'waitlist': waitlist}
+    else:
+        size = None
+
+    if course.select_one('.codes.gov_codes'):
+        tags = [{'name': code.get_text(), 'code': parse_qs(code.get('href')).get('other_code[]', None)}
+                for code in course.select('.codes.gov_codes a')]
+    else:
+        tags = []
+
+    if course.select_one('.codes.overlays'):
+        requirements = [{'name': code.get_text(), 'code': parse_qs(code.get('href')).get('requirements[]', None)}
+                        for code in course.select('.codes.overlays a')]
+    else:
+        requirements = []
+
+    if course.select_one('.credits'):
+        credits_el = course.select_one('.credits')
+        credit_count = float(re.search(r'^([\d.])+', credits_el.get_text()).group(1))
+
+        if credits_el.select_one('abbr'):
+            scnc = credits_el.select_one('abbr').get_text()
+        else:
+            scnc = None
+    else:
+        credit_count = None
+        scnc = None
+
+    if course.select_one('.textbooks'):
+        el = course.select_one('.textbooks')
+        synonym = re.search(r'Synonym: (\d+)', el.get_text()).group(1)
+    else:
+        synonym = None
+
+    if course.select_one('.schedule'):
+        # Start and end times for courses that have set times
+        # Account for classes without set times
+        schedule = course.select_one('.schedule')
+
+        # locations = course.select('.locations a')
+
+        # offerings = []
+        # times = {0: [], 1: [], 2: [], 3: [], 4: []}
+        # for tr in schedule.select('tr')[1:]:
+        #     for i, td in enumerate(tr.select('td')):
+        #         times[i].append({
+        #             'start': td.select_one('.start').get_text() if td.select_one('.start') else None,
+        #             'end': td.select_one('.end').get_text()if td.select_one('.end') else None,
+        #         })
+
+        offerings = str(schedule.parent)
+    else:
+        offerings = ''
+
+    return {
+        'id': f'{subject} {number}',
+        'title': title,
+        'subject': subject,
+        'number': number,
+        'instructors': instructors,
+        'summary': summary,
+        'prerequisites': prereq,
+        'comments': comments,
+        'credits': credit_count,
+        'offerings': offerings,
+        'requirements': requirements,
+        'scnc': scnc,
+        'size': size,
+        'status': status,
+        'synonym': synonym,
+        'tags': tags,
+    }
+
+
+def fetch_subject_for_term(*, term, subject):
     html_string = f'https://apps.carleton.edu/campus/registrar/schedule/enroll/?term={term}&subject={subject}'
 
     # Course listings for subject during term provided
     html = requests.get(html_string).text
 
+    return html
+
+
+def extract_courses(*, html):
+    """ Returns dict object with course number, course name, and start/end times for each course
+    Finds course info based on the academic term and subject chosen (in this case, Winter 2018)
+    """
     soup = BeautifulSoup(html, 'html5lib')
 
     # Creates list of all items with course as class attribute, excluding related courses
-    course_summary = soup.find_all("div", class_="course")
-    for course in course_summary:
-        course_num = course.find(class_="coursenum").get_text()
-
-        # Finds title attribute within each course
-        title = course.find(class_="title").get_text()
-
-        # Only takes the actual name of the course, which is next to the coursenum attribute
-        # but not within its own tag
-        course_name = 'n/a'
-        for _ in title:
-            course_name = course.find(class_="coursenum").next_sibling
-
-        if not course_name:
-            raise Exception('no course name found!')
-
-        # Add info to list associated with key
-        specific_info = {}
-        if course_num.find(subject) > -1:
-            specific_info['course_num'] = course_num
-            specific_info['title'] = course_name.strip()
-            if course.find(class_="faculty") is not None:
-                faculty = course.find(class_="faculty").get_text()
-                specific_info['faculty'] = faculty.strip()
-                if course.find(class_="faculty").next_sibling is not None:
-                    summary = course.find(class_="faculty").next_sibling
-                    specific_info['summary'] = str(summary)
-                else:
-                    specific_info['summary'] = "n/a"
-            else:
-                specific_info['faculty'] = "n/a"
-            if course.find(class_="status") is not None:
-                enrollment = course.find(class_="status").get_text()
-                # specific_info['enrollment'] = enrollment
-                registered = re.findall(r'(?<=Registered: ).*?(?=,)', enrollment)[0]
-                size = re.findall(r'(?<=Size: ).*?(?=,)', enrollment)[0]
-                # print registered
-                specific_info['registered'] = registered
-                specific_info['size'] = size
-                # print enrollment
-            else:
-                specific_info['registered'] = "n/a"
-                specific_info['size'] = "n/a"
-
-            # Start and end times for courses that have set times
-            # Account for classes without set times
-            if course.find(class_="start") is not None:
-                start_time = course.find("span", {"class": "start"}).get_text()
-                end_time = course.find(class_="end").get_text()
-                specific_info['start_time'] = start_time
-                specific_info['end_time'] = end_time
-            else:
-                specific_info['start_time'] = "n/a"
-                specific_info['end_time'] = "n/a"
-
-            course_info.append(specific_info)
-
-    return course_info
+    exact_courses = soup.select('#enrollModule h3 ~ .courses .course')
+    for course in exact_courses:
+        # print(course)
+        yield process_course(course)
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('TERM', action='store', nargs='*', metavar='TERM',
+    parser.add_argument('command', action='store', choices=['fetch', 'extract', 'bundle'],
+                        help='Which command to execute')
+    parser.add_argument('terms', action='store', nargs='*', metavar='TERM',
                         help='A term, like 18WI or 15SP')
     parser.add_argument('--dest', action='store',
                         help='The folder to output data files to', default='../course-data')
@@ -185,13 +253,12 @@ def main():
                         help='Print the known terms, then exit')
     parser.add_argument('--delay', action='store', type=int, default=1,
                         help='Control the delay between term/subject fetches, in seconds (be nice)')
-    parser.add_argument('--first-term', action='store', default='99WI', metavar='TERM',
+    parser.add_argument('--first-term', action='store', metavar='TERM',
                         help='Fetch terms from the given term until --last-term')
     parser.add_argument('--last-term', action='store', metavar='TERM',
                         help='Fetch terms from --first-term until the given term')
 
     args = parser.parse_args()
-    args.terms = args.TERM
 
     if not args.subjects:
         args.subjects = fetch_subjects()
@@ -210,16 +277,63 @@ def main():
         [print(s) for s in args.terms]
         return
 
-    for term in args.terms:
-        for subject in args.subjects:
-            print(f'fetching term "{term}", subject "{subject}"')
+    root = Path(args.dest) if args.dest else Path('..') / 'course-data'
 
-            course_info = fetch_courses(term=term, subject=subject)
+    if args.command == 'fetch':
+        for term in args.terms:
+            for subject in args.subjects:
+                print(f'fetching term "{term}", subject "{subject}"', file=sys.stderr)
 
-            with open(f'../course-data/{term}-{subject}.json', 'w') as outfile:
-                json.dump(course_info, outfile)
+                folder = root / 'indices' / term / subject
+                folder.mkdir(parents=True, exist_ok=True)
 
-            time.sleep(args.delay)
+                html = fetch_subject_for_term(term=term, subject=subject)
+                with open(folder / '_index.html', 'w') as outfile:
+                    outfile.write(html)
+                    outfile.write('\n')
+
+                time.sleep(args.delay)
+    elif args.command == 'extract':
+        index_dir = root / 'indices'
+        files_dir = root / 'courses'
+        for term in [d for d in index_dir.glob('*') if d.is_dir()]:
+            for subject in [d for d in term.glob('*') if d.is_dir()]:
+                print(f'extracting term "{term.name}", subject "{subject.name}"', file=sys.stderr)
+                with open(subject / '_index.html', 'r') as infile:
+                    html = infile.read()
+
+                out_dir = files_dir / term.name / subject.name
+                out_dir.mkdir(parents=True, exist_ok=True)
+
+                for course in extract_courses(html=html):
+                    with open(out_dir / f'{course["id"]}.json', 'w') as outfile:
+                        json.dump(course, outfile, indent='\t', sort_keys=True)
+                        outfile.write('\n')
+    elif args.command == 'bundle':
+        terms = {}
+
+        files_dir = root / 'courses'
+        for term in [d for d in files_dir.glob('*') if d.is_dir()]:
+            subjects = {}
+
+            for subject in [d for d in term.glob('*') if d.is_dir()]:
+                print(f'bundling term "{term.name}", subject "{subject.name}"', file=sys.stderr)
+
+                filenames = [file for file in subject.glob('*.json')]
+
+                courses = []
+                for file in filenames:
+                    with open(file, 'r') as infile:
+                        courses.append(json.load(infile))
+
+                subjects[subject.name] = courses
+
+            terms[term.name] = subjects
+
+        with open(root / 'all.json', 'w') as outfile:
+            print(f'saving all-term bundle')
+            json.dump(terms, outfile, indent='\t', sort_keys=True)
+            outfile.write('\n')
 
 
 if __name__ == '__main__':
