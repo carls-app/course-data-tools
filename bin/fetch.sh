325b9aaf8308e7a0cf3cc4ac8d8c15c7eb653059
Hawken Rives <hawkrives@gmail.com>
2018-04-21 12:41:19 -0500

    only run two at a time on Travis
    since it's just dual-core

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -9,13 +9,13 @@ function terms() {
         for s in WI SP FA; do
             echo "$y$s"
         done
-        # -n1 to only pass one argument from stdin to the process
-        # -P3 to run 3 concurrent fetch processes
     done
 }
 
-terms | xargs -t -n1 -P3 -- python3 ../read-enroll.py fetch
-terms | xargs -t -n1 -P3 -- python3 ../read-enroll.py extract
+# -n1 to only pass one argument from stdin to the process
+# -P2 to run 2 concurrent fetch processes
+terms | xargs -t -n1 -P2 -- python3 ../read-enroll.py fetch
+terms | xargs -t -n1 -P2 -- python3 ../read-enroll.py extract
 
 git add .
 git commit -m "course data update $(date)" || (echo "No updates found." && exit 0)
